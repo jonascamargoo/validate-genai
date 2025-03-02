16da603c405d03c22f7fd2ad98de72574097dc03
Jonas <jonascamargo999@gmail.com>
2025-03-02 17:51:16 -0300

    feat: adiciona dataset da b2b

--- app/datasets/dataset_b2b.py
+++ app/datasets/dataset_b2b.py
@@ -1,17 +1,16 @@
-# import pandas as pd
+# dataset real de avaliações de produtos da B2W (Americanas, Submarino, Shoptime). Link para download: https://github.com/americanas-tech/b2w-reviews01
 
-# # Opção 2: Dataset b2w-reviews
-# # Este é um dataset real de avaliações de produtos da B2W (Americanas, Submarino, Shoptime)
-# # Link para download: https://github.com/americanas-tech/b2w-reviews01
+import pandas as pd
 
-# # Transforma as notas (1-5) em sentimentos: 
-# # - Notas > 3 são positivas (1)
-# # - Notas < 3 são negativas (0)
-# # - Nota 3 é considerada neutra (2)
+# Carrega o dataset de reviews
+df = pd.read_csv("B2W-Reviews01.csv", encoding="utf-8")
 
+# Converter a nota (rating) em categorias de sentimento:
+#   - Positivo (1) para ratings acima de 3
+#   - Neutro (2) para ratings igual a 3
+#   - Negativo (0) para ratings abaixo de 3
+df['sentiment'] = df['overall_rating'].apply(lambda x: 1 if x > 3 else (0 if x < 3 else 2))
+# print(df.head())
 
-# df = pd.read_csv('b2w-reviews01.csv', encoding='utf-8')  # Carrega o arquivo CSV
-# df['sentiment'] = df['rating'].apply(lambda x: 1 if x > 3 else (0 if x < 3 else 2))
-# df = df.rename(columns={'text': 'review_text'})  # Renomeia a coluna para padronizar
-
-    
+df_subset = df[['overall_rating', 'review_title', 'review_text']]
+print(df_subset)
